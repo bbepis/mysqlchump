bb15d6e5eff1a5a0b2f6fe285a5fa2a08a0ed292
Bepis <36346617+bbepis@users.noreply.github.com>
2019-09-05 00:30:49 +1000

    Initial code commit

---
+++ mysqlchump/Dumper.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace mysqlchump
+{
+	public class Dumper
+	{
+		public MySqlConnection Connection { get; }
+
+		public Dumper(MySqlConnection connection)
+		{
+			Connection = connection;
+		}
+
+		public async Task WriteTableSchemaAsync(string table, Stream outputStream)
+		{
+			using (var writer = new StreamWriter(outputStream, Encoding.UTF8, 4096, true))
+			using (var createTableCommand = new MySqlCommand($"SHOW CREATE TABLE `{table}`", Connection))
+			using (var reader = await createTableCommand.ExecuteReaderAsync())
+			{
+				if (!await reader.ReadAsync())
+					throw new ArgumentException($"Could not find table `{table}`");
+
+				string createSql = (string)reader["Create Table"];
+
+				createSql = createSql.Replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS");
+
+				await writer.WriteAsync(createSql);
+				await writer.WriteAsync(";\n\n\n");
+			}
+		}
+
+		public async Task WriteInsertQueries(string table, string query, Stream outputStream, MySqlTransaction transaction = null)
+		{
+			int counter = 0;
+			int splitCounter = 1024;
+
+			var bodyBuilder = new StringBuilder();
+			Task ioTask = null;
+
+			using (var writer = new StreamWriter(outputStream, Encoding.UTF8, 4096, true))
+			using (var selectCommand = new MySqlCommand(query, Connection, transaction))
+			using (var reader = await selectCommand.ExecuteReaderAsync())
+			{
+				bool start = true;
+
+				DbColumn[] columns = null;
+
+				while (await reader.ReadAsync())
+				{
+					if (counter++ % splitCounter == 0 || start)
+					{
+						if (bodyBuilder.Length > 0)
+						{
+							if (ioTask != null)
+								await ioTask;
+
+							ioTask = writer.WriteAsync(bodyBuilder.ToString());
+							bodyBuilder.Clear();
+						}
+
+						if (start)
+						{
+							// Read schema
+							columns = reader.GetColumnSchema().AsEnumerable().ToArray();
+						}
+						else
+						{
+							bodyBuilder.AppendLine(";");
+						}
+
+						bodyBuilder.AppendLine($"INSERT INTO `{table}` ({string.Join(", ", columns.Select(column => $"`{column.ColumnName}`"))}) VALUES");
+
+						start = false;
+					}
+					else
+					{
+						bodyBuilder.AppendLine(",");
+					}
+
+					bodyBuilder.Append("(");
+
+					bool rowStart = true;
+					foreach (var column in columns)
+					{
+						if (!rowStart)
+							bodyBuilder.Append(", ");
+
+						bodyBuilder.Append(GetMySqlStringRepresentation(column, reader[column.ColumnName]));
+
+						rowStart = false;
+					}
+
+					bodyBuilder.Append(")");
+				}
+
+				bodyBuilder.Append(";\n\n");
+
+				if (ioTask != null)
+					await ioTask;
+
+				await writer.WriteAsync(bodyBuilder.ToString());
+			}
+		}
+
+		private static string GetMySqlStringRepresentation(DbColumn column, object value)
+		{
+			if (value == null || value == DBNull.Value)
+				return "NULL";
+
+			var columnType = column.DataType;
+
+			if (columnType == typeof(byte)
+				|| columnType == typeof(sbyte)
+				|| columnType == typeof(ushort)
+				|| columnType == typeof(short)
+				|| columnType == typeof(uint)
+				|| columnType == typeof(int)
+				|| columnType == typeof(ulong)
+				|| columnType == typeof(long)
+				|| columnType == typeof(float)
+				|| columnType == typeof(double)
+				|| columnType == typeof(decimal))
+			{
+				return value.ToString();
+			}
+
+			if (columnType == typeof(bool))
+				return (bool)value ? "1" : "0";
+
+			if (columnType == typeof(string))
+				return "'" + MySqlHelper.EscapeString(value.ToString())
+										.Replace("\r", "\\r")
+										.Replace("\n", "\\n")
+						   + "'";
+
+			if (columnType == typeof(byte[]))
+				return "_binary 0x" + ByteArrayToString((byte[])value);
+
+			throw new SqlTypeException($"Could not represent type: {column.DataTypeName}");
+		}
+
+		private static string ByteArrayToString(byte[] ba)
+		{
+			StringBuilder hex = new StringBuilder(ba.Length * 2);
+			foreach (byte b in ba)
+				hex.AppendFormat("{0:x2}", b);
+			return hex.ToString();
+		}
+	}
+}
---
+++ mysqlchump/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace mysqlchump
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var arguments = Arguments.Parse(args);
+
+			if (args.Length == 0 || arguments.Switches.ContainsKey("help"))
+			{
+				Console.WriteLine("Usage: mysqlchump --table <table> --connectionString <connection string> [--select <select sql statement>] [--append] <output file>");
+				return;
+			}
+
+			string table = arguments["table"];
+			string connectionString = arguments["connectionString"];
+			string selectStatement = arguments["select"] ?? $"SELECT * FROM `{table}`";
+			string outputFile = arguments.Values[0];
+
+			if (table == null)
+				throw new ArgumentException("Expecting argument: --table");
+
+			if (connectionString == null)
+				throw new ArgumentException("Expecting argument: --connectionString");
+
+			if (outputFile == null)
+				throw new ArgumentException("Expecting argument: output file");
+
+			bool append = arguments.Switches.ContainsKey("append");
+
+
+			using (var fileStream = new FileStream(outputFile, append ? FileMode.Append : FileMode.Create))
+			using (var connection = new MySqlConnection(connectionString))
+			{
+				await connection.OpenAsync();
+
+				var dumper = new Dumper(connection);
+				await dumper.WriteTableSchemaAsync(table, fileStream);
+
+				using (var transaction = await connection.BeginTransactionAsync(IsolationLevel.RepeatableRead))
+				{
+					await dumper.WriteInsertQueries(table, selectStatement, fileStream, transaction);
+				}
+			}
+		}
+	}
+}
