0f30f599724af9123d84604a23892ac9e5f8227f
Bepis <36346617+bbepis@users.noreply.github.com>
2019-11-29 23:18:06 +1100

    Allow multiple tables to be dumped at once

--- mysqlchump/Program.cs
+++ mysqlchump/Program.cs
@@ -14,7 +14,9 @@ namespace mysqlchump
 
 			if (args.Length == 0 || arguments.Switches.ContainsKey("help"))
 			{
-				Console.WriteLine("Usage: mysqlchump --table <table> --connectionString <connection string> [--select <select sql statement>] [--append] (--stdout | <output file>)");
+				Console.WriteLine("Usage:");
+				Console.WriteLine("mysqlchump --table <table> --connectionString <connection string> [--select <select sql statement>] [--append] (--stdout | <output file>)");
+				Console.WriteLine("mysqlchump --tables <comma separated table names> --connectionString <connection string> [--select <select sql statement>] [<output folder>]");
 				return;
 			}
 
@@ -23,47 +25,91 @@ namespace mysqlchump
 
 			string table = arguments["table"];
 			string connectionString = arguments["connectionString"];
-			string selectStatement = arguments["select"] ?? $"SELECT * FROM `{table}`";
+			string selectStatement = arguments["select"] ?? "SELECT * FROM `{table}`";
 			string outputFile = null;
+
+			string[] tables = arguments["tables"]?.Split(',', StringSplitOptions.RemoveEmptyEntries);
 			
 			if (!standardOut)
 				outputFile = arguments.Values[0];
 
-			if (table == null)
-				throw new ArgumentException("Expecting argument: --table");
+			if (table == null && tables == null)
+				throw new ArgumentException("Expecting argument: --table or --tables");
 
 			if (connectionString == null)
 				throw new ArgumentException("Expecting argument: --connectionString");
 
-			if (!standardOut && outputFile == null)
-				throw new ArgumentException("Expecting argument: output file");
+			bool singleTableMode = table != null;
 
+			if (singleTableMode)
+			{
+				if (!standardOut && outputFile == null)
+					throw new ArgumentException("Expecting argument: output file");
 
-			Stream stream;
-
-			if (standardOut)
-				stream = Console.OpenStandardOutput();
+				await DumpSingleTable(table, selectStatement, connectionString, outputFile, standardOut, append);
+			}
 			else
-				stream = new FileStream(outputFile, append ? FileMode.Append : FileMode.Create);
+			{
+				string outputFolderPath = arguments.Values.Count > 0
+					? arguments.Values[0]
+					: Environment.CurrentDirectory;
 
+				await DumpMultipleTables(tables, selectStatement, connectionString, outputFolderPath);
+			}
+		}
+
+		static async Task DumpTableToStream(string table, string selectStatement, MySqlConnection connection, Stream stream)
+		{
+			var dumper = new Dumper(connection);
+			await dumper.WriteTableSchemaAsync(table, stream);
+
+			using (var transaction = await connection.BeginTransactionAsync(IsolationLevel.RepeatableRead))
+			{
+				await dumper.WriteInsertQueries(table, selectStatement, stream, transaction);
+			}
+		}
+
+		static async Task DumpSingleTable(string table, string selectStatement, string connectionString, string outputFile, bool standardOut, bool append)
+		{
+			var stream = standardOut
+				? Console.OpenStandardOutput()
+				: new FileStream(outputFile, append ? FileMode.Append : FileMode.Create);
+
+			string formattedQuery = selectStatement.Replace("{table}", table);
 
 			using (var connection = new MySqlConnection(connectionString))
 			{
 				await connection.OpenAsync();
 
-				var dumper = new Dumper(connection);
-				await dumper.WriteTableSchemaAsync(table, stream);
-
-				using (var transaction = await connection.BeginTransactionAsync(IsolationLevel.RepeatableRead))
-				{
-					await dumper.WriteInsertQueries(table, selectStatement, stream, transaction);
-				}
+				await DumpTableToStream(table, formattedQuery, connection, stream);
 			}
 
 			await stream.FlushAsync();
 
 			if (!standardOut)
 				stream.Dispose();
+		}
+
+		static async Task DumpMultipleTables(string[] tables, string selectStatement, string connectionString, string outputFolder)
+		{
+			var dateTime = DateTime.Now;
+
+			using (var connection = new MySqlConnection(connectionString))
+			{
+				await connection.OpenAsync();
+
+				foreach (var table in tables)
+				{
+					string dumpFileName = $"dump_{dateTime:yyyy-MM-dd_hh-mm-ss}_{table}.sql";
+
+					using (var stream = new FileStream(Path.Combine(outputFolder, dumpFileName), FileMode.CreateNew))
+					{
+						string formattedQuery = selectStatement.Replace("{table}", table);
+
+						await DumpTableToStream(table, formattedQuery, connection, stream);
+					}
+				}
+			}
 		}
 	}
 }
